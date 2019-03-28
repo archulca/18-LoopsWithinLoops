55c7b43384995ec4e3d80f986a189f22fdb80b93
Carla Archuleta <archulca@rose-hulman.edu>
2019-03-28 13:51:03 -0400

    DONE #3

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -164,8 +164,8 @@ def decreasing_exclamation_marks(m, n):
     #   in this or the other problems in this module, as doing so
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
-    for k in range (m):
-        for j in range(m-k):
+    for k in range (m,n-1,-1):
+        for j in range(k):
             print('!', end='')
         print()
 
